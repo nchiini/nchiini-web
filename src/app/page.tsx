75dfd9e8a75487960c90d2e182699bb1829f64cc
casaemiro <silasmagho18@gmail.com>
2024-03-16 12:14:02 +0100

    added image to meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           content={`Nchiini is a web app designed to assist people in learning about their culture in an engaging and interactive manner. The app offers a user-friendly interface and a personalized learning experience to help users explore and understand different aspects of their culture`}
         />
         <meta property="og:type" content="nchiini" />
-        <meta property="og:image" content={`/unnamed.jpg`} />
-        {/* <meta property="og:image" content={`https://firebasestorage.googleapis.com/v0/b/nchiini-86492.appspot.com/o/unnamed.jpg?alt=media&token=2de34df1-d3f1-4a94-985f-0d083fd6aa12`} /> */}
+        {/* <meta property="og:image" content={`/unnamed.jpg`} /> */}
+        <meta property="og:image" content={`https://firebasestorage.googleapis.com/v0/b/nchiini-86492.appspot.com/o/unnamed.jpg?alt=media&token=2de34df1-d3f1-4a94-985f-0d083fd6aa12`} />
 
         <meta property="og:image:width" content="300" />
         <meta property="og:image:height" content="300" />
