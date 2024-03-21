2b36651c2108fc8d935423c6986440d8034e672c
casaemiro <silasmagho18@gmail.com>
2024-03-21 13:43:09 +0100

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,20 @@ export const metadata: Metadata = {
   title: "Nchiini",
   description:
     "Nchiini is a web app designed to assist people in learning about their culture in an engaging and interactive manner. The app offers a user-friendly interface and a personalized learning experience to help users explore and understand different aspects of their culture.",
+  applicationName: "Nchiini",
+  openGraph: {
+    type: "website",
+    url: "https://nchiini-web.vercel.app/",
+    title: "Nchiini | Culture",
+    description:
+      "Nchiini is a web app designed to assist people in learning about their culture in an engaging and interactive manner. The app offers a user-friendly interface and a personalized learning experience to help users explore and understand different aspects of their culture",
+    siteName: "Nchiini",
+    images: [
+      {
+        url: "https://firebasestorage.googleapis.com/v0/b/nchiini-86492.appspot.com/o/unnamed.jpg?alt=media&token=2de34df1-d3f1-4a94-985f-0d083fd6aa12",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
